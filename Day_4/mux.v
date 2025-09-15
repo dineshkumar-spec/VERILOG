662f687917df648180ffd323db65c42ad6355c5a
dineshkumar-spec <dineshkumarperiyasamy11@gmail.com>
2025-09-15 12:38:01 +0530

    Add files via upload

---
+++ Day_4/mux.v
@@ -0,0 +1,13 @@
+module mux(
+input [1:0]s,
+input [3:0]d,
+output reg y);
+always@(*)begin
+case(s)
+2'b00:y=d[0];
+2'b01:y=d[1];
+2'b10:y=d[2];
+2'b11:y=d[3];
+endcase
+end
+endmodule
---
+++ Day_4/tb_mux.v
@@ -0,0 +1,14 @@
+module tb_mux;
+reg [1:0]s;
+reg [3:0]d;
+wire y;
+mux dut (.s(s), .d(d), .y(y));
+initial begin
+$monitor("time=%0t|s=%b|d=%b|y=%b",$time,s,d,y);
+d=4'b1101; s=2'b00; #5;
+s=2'b01; #5;
+s=2'b10; #5;
+s=2'b11; #5;
+$finish;
+end
+endmodule
