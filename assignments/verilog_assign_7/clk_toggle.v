ece3052bb66d90662c24c19c2d6ac78d68d9cd84
dineshkumar-spec <dineshkumarperiyasamy11@gmail.com>
2025-09-17 17:40:27 +0530

    Create clk_toggle.v

---
+++ assignments/verilog_assign_7/clk_toggle.v
@@ -0,0 +1,20 @@
+//Create a task to toggle a clock signal 10 times with a delay of 5 time units.
+module clk_toggle;
+reg clk;
+task toggle;
+begin
+clk=0;
+#5;
+$display("time=%0t|clk=%b",$time,clk);
+clk=1;
+#5;
+$display("time=%0t|clk=%b",$time,clk);
+end
+endtask
+initial begin
+repeat(10)begin
+toggle();
+end
+#50 $finish;
+end
+endmodule
