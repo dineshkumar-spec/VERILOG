a73fcbd4f073fe58fecbe3c8d3150b12c19faff2
dineshkumar-spec <dineshkumarperiyasamy11@gmail.com>
2025-09-17 11:38:22 +0530

    Create factorial.v

---
+++ assignments/verilog_assign_7/factorial.v
@@ -0,0 +1,22 @@
+//Write a task that calculates the factorial of a number using a loop (not recursion).
+module fact;
+reg [7:0]n;
+reg [15:0]result;
+
+task factorial;
+        input [7:0]num;
+        output[15:0]fact;
+        integer i;
+begin
+fact=1;
+for(i=1;i<=num;i=i+1)begin
+fact = fact*i;
+end
+end
+endtask
+initial begin
+n=5;
+factorial(n,result);
+$display("Factorial of a number %0d = %0d",n,result);
+end
+endmodule
