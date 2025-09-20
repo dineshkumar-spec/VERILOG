657cf7d6820f85d97dd1e609304c075e77537bec
dineshkumar-spec <dineshkumarperiyasamy11@gmail.com>
2025-09-20 10:40:32 +0530

    Create 41.display.v

---
+++ assignments/verilog_assign_7/SYSTEM TASKS & SIMULATION CONTROL/41.display.v
@@ -0,0 +1,14 @@
+module signal;
+reg data;
+reg clk;
+initial begin
+clk = 0;
+#7 data = 1;
+#10 data = 0;
+end
+always #5 clk = ~clk;
+initial begin
+$display("time=%0t|data=%b|clk=%b",$time,data,clk);
+#50 $finish;
+end
+endmodule
