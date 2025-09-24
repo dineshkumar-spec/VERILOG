ab155027db767ef326a70c90de647d9b12b6c9dd
dineshkumar-spec <dineshkumarperiyasamy11@gmail.com>
2025-09-24 10:54:17 +0530

    Create race.v

---
+++ Day_8/race.v
@@ -0,0 +1,20 @@
+module race;
+reg a,b;
+
+initial begin
+a=1;b=0;
+end
+initial begin
+#5;a=b;
+end
+initial begin
+#5;a=0;
+end
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars;
+$monitor("time=%0t|a=%b|b=%b",$time,a,b);
+end
+endmodule
+/*time=0|a=1|b=0
+time=5|a=0|b=0*/
