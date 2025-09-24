4e1ed99cb859e471ef08b56a5adcb40910e789bb
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-09-24 18:30:12 +0530

    Update bcd2excess.v

--- Day_8/bcd2excess.v
+++ Day_8/bcd2excess.v
@@ -1,4 +1,12 @@
+//dataflow level
 //bcd to excess
+module bcd_exce(input[3:0]b,output [3:0]e);
+assign e[3]=b[3]|b[2]&b[1]|b[2]&b[0];
+assign e[2]=~b[2]&b[0]|~b[2]&b[1]|b[2]&~b[1]&~b[0];
+assign e[1]=~b[1]&~b[0]|b[1]&b[0];
+assign e[0]=~b[0];
+endmodule
+//bcd to excess-behavioural level
 module bcd_exce
 (input[3:0]b,
 output reg [3:0]e);
