46221b7dce1523dbfc41e8e9ad8c7379ceae7c7d
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-09-25 11:49:57 +0530

    Update encoder 8to3.v

--- Day_9/encoder 8to3.v
+++ Day_9/encoder 8to3.v
@@ -13,6 +13,29 @@ default:out=3'b000;
 endcase
 end
 endmodule
+//if else
+module encoder(input [7:0]d,output reg [2:0]out);
+always@(*)begin
+if (d==8'b10000000)
+        out=3'b000;
+else if (d==8'b01000000)
+        out=3'b001;
+else if (d==8'b00100000)
+        out=3'b010;
+else if (d==8'b00010000)
+        out=3'b011;
+else if (d==8'b00001000)
+        out=3'b100;
+else if (d==8'b00000100)
+        out=3'b101;
+else if (d==8'b00000010)
+        out=3'b110;
+else if (d==8'b00000001)
+        out=3'b111;
+else
+        out=3'b000;
+end
+endmodule
 
 //tb
 module tb_encoder;
