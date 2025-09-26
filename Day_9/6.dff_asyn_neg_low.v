d67d3d8b1a9beb3f47c43ddbc3e8590f9455a861
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-09-26 19:09:23 +0530

    Create 6.dff_asyn_neg_low.v

---
+++ Day_9/6.dff_asyn_neg_low.v
@@ -0,0 +1,34 @@
+module dff_asyn(input clk,
+input reset,
+input d,
+output reg q);
+always@(negedge clk or negedge reset)begin
+        if(reset)
+                q<=1'b0;
+        else
+               q<=d;
+end
+endmodule
+//tb
+module tb_dff_asyn;
+reg clk,reset,d;
+wire q;
+
+dff_asyn dut (.clk(clk),.reset(reset),.d(d),.q(q));
+initial begin
+clk=0;
+forever #5 clk = ~clk;
+end
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,tb_dff_asyn);
+$monitor("time=%0t|clk=%b|reset=%b|d=%b|q=%b",$time,clk,reset,d,q);
+reset=1;d=0;#5;
+reset=0;d=0;#5;
+d=1;#5;
+d=1;#5;
+d=0;#5;
+d=1;#5;
+reset=1;d=0;
+$finish;
+end
