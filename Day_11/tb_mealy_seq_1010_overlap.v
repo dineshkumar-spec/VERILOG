4ff624e3569a0dd25057e392572bb4e03bed6069
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-10-06 22:08:12 +0530

    Create tb_mealy_seq_1010_overlap.v

---
+++ Day_11/tb_mealy_seq_1010_overlap.v
@@ -0,0 +1,29 @@
+module tb_seq_1010;
+reg clk;
+reg rst;
+reg x;
+wire z;
+seq_1010 dut (.x(x), .clk(clk), .rst(rst), .z(z));
+initial begin
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars;
+$monitor("time=%0t|clk=%b|x=%b|z=%b",$time,clk,x,z);
+rst=0;x=0;
+#10 rst=1;
+
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+
+#20 $finish;
+end
+endmodule
