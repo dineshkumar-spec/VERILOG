320e45de847e8f5d464b1b80f81a69db97493ba6
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-10-15 00:26:35 +0530

    Create 8.tb_seq_111.v

---
+++ assignments/verilog_assign_8/8.tb_seq_111.v
@@ -0,0 +1,30 @@
+module tb_seq_111;
+reg clk;
+reg rst;
+reg x;
+wire z;
+seq_111 dut (.clk(clk), .rst(rst), .x(x), .z(z));
+initial begin
+clk=0;
+forever #5 clk=~clk;
+end
+always@(posedge clk)
+$display("time=%0t|clk=%b|x=%b|z=%b",$time,clk,x,z);
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars;
+//$monitor("time=%0t|clk=%b|x=%b|z=%b",$time,clk,x,z);
+rst=0;x=0;
+#10 rst=1;
+#5 x=1;
+#5 x=1;
+#5 x=1;
+#5 x=0;
+#5 x=1;
+#5 x=1;
+#5 x=1;
+#5 x=0;
+#5 x=0;
+#100 $finish;
+end
+endmodule
