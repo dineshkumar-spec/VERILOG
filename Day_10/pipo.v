079ba39bcb4e9432950711783ea91a2df167baf0
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-02 18:29:03 +0530

    Update pipo.v

--- Day_10/pipo.v
+++ Day_10/pipo.v
@@ -1,43 +1,32 @@
-module pipo(
-input [3:0]pi,
-input clk,rst,
-output reg [3:0]po);
-always@(posedge clk or posedge rst)begin
-        if(rst)begin
-                po<=4'b0000;end
-                else begin
+module pipo(input [3:0]pi,input clk,reset,output reg [3:0]po);
+always@(posedge clk)begin
+if(reset)
+po<=4'b000;
+else
 po<=pi;
 end
-end
 endmodule
-//tb
-module tb_pipo;
-reg [3:0]pi;
+
+module tb;
+reg [3:0] pi;
 reg clk;
-reg rst;
+reg reset;
 wire [3:0]po;
-pipo dut (.pi(pi), .clk(clk), .rst(rst), .po(po));
+pipo dut (pi,clk,reset,po);
 initial begin
-clk=1;
-$dumpfile("wave.vcd");
-$dumpvars;
-forever #5 clk=~clk;
+clk=0;
+forever #5 clk= ~clk;
 end
 initial begin
-rst=1;
-pi=4'b1111;#5;
-rst=0;
-pi=4'b1011;#5;
-pi=4'b1010;#5;
-rst=1;pi=4'b0011;
+$dumpfile("pipo.vcd");
+$dumpvars;
+$monitor("time=%0t|clk=%b|reset=%b|pi=%b|po=%b",$time,clk,reset,pi,po);
+reset=1;#10;
+reset=0;
+pi=4'b1010;#10;
+reset=1;
+pi=4'b1111;
+#100;
 $finish;
 end
-initial begin
-$monitor("time=%0t|clk=%b|reset=%b|pi=%b|po=%b",$time,clk,rst,pi,po);
-end
 endmodule
-/*time=0|clk=1|reset=1|pi=1111|po=0000
-time=5|clk=0|reset=0|pi=1011|po=0000
-time=10|clk=1|reset=0|pi=1010|po=1010
-pipo.v:34: $finish called at 15 (1s)
-time=15|clk=0|reset=1|pi=0011|po=0000*/
